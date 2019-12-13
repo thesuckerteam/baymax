a4b5b1bd176d0e71cc8c5a94bd425a74fbc7caaf
Oskar Svensson <osksvn@outlook.com>
2019-12-13 16:58:41 +0700

    added trello api functions

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/addCard', function(req,res, next){
+router.get('/addCardTODO', function(req,res, next){
   trello.addCardTODO(function(){
 
   })
--- utility/trello.js
+++ utility/trello.js
@@ -15,6 +15,48 @@ exports.addCardTODO = function(name, desc, callback){
         })
 }
 
+exports.addCardDOING = function(name, desc, callback){
+    trello.addCard(name, desc, '5df342eb9a2497638c2e7d23',
+    function(error, trelloCard){
+        if (error) console.log(error)
+        else console.log('Added card : ' + trelloCard)
+        callback()
+    })
+}
+
+exports.addCardDONE = function(name, desc, callback){
+    trello.addCard(name, desc, '5df342eb9a2497638c2e7d24',
+    function(error, trelloCard){
+        if (error) console.log(error)
+        else console.log('Added card : ' + trelloCard)
+        callback()
+    })
+}
+
+exports.deleteCard = function(cardId, callback){
+    trello.deleteCard(cardId, function(error, result){
+        if (error) console.log(error)
+        else console.log('deleted card: ' + result)
+        callback()
+    })
+}
+
+exports.updateCardName = function(cardId, name, callback){
+    trello.updateCardName(cardId, name, function(error, result){
+        if (error) console.log(error)
+        else console.log('Updated card: ' + result)
+        callback()
+    })
+}
+
+exports.updateListName = function(listId, name, callback){
+    trello.renameList(listId, name, function(error, result){
+        if (error) console.log(error)
+        else console.log('Updated list name: ' + result)
+        callback()
+    })
+}
+
 exports.getBoards = function(callback) {
 
     trello.getBoards('5dd79b7456259d09ca4fac1a', function(error, result){
