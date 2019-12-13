94d1e0c14a864233ca16f4dba70e9f25de6f6aba
Oskar Svensson <osksvn@outlook.com>
2019-12-13 18:37:43 +0700

    added all calls for trello api

--- utility/trello.js
+++ utility/trello.js
@@ -2,7 +2,7 @@ var Trello = require('trello');
 
 var trello = new Trello('31d325be1f01a6fc44dc51db6dc63f88', '6d80a3eded49d7a88b16efc5b601745b88a48637159651f6885afc01bf0dde5d');
 
-exports.addCardTODO = function(name, desc, callback){
+exports.addCardTODO = function (name, desc, callback) {
 
     trello.addCard(name, desc, '5df342eb9a2497638c2e7d22',
         function (error, trelloCard) {
@@ -15,68 +15,94 @@ exports.addCardTODO = function(name, desc, callback){
         })
 }
 
-exports.addCardDOING = function(name, desc, callback){
+exports.addCardDOING = function (name, desc, callback) {
     trello.addCard(name, desc, '5df342eb9a2497638c2e7d23',
-    function(error, trelloCard){
-        if (error) console.log(error)
-        else console.log('Added card : ' + trelloCard)
-        callback()
-    })
+        function (error, trelloCard) {
+            if (error) console.log(error)
+            else console.log('Added card : ' + trelloCard)
+            callback()
+        })
 }
 
-exports.addCardDONE = function(name, desc, callback){
+exports.addCardDONE = function (name, desc, callback) {
     trello.addCard(name, desc, '5df342eb9a2497638c2e7d24',
-    function(error, trelloCard){
-        if (error) console.log(error)
-        else console.log('Added card : ' + trelloCard)
-        callback()
-    })
+        function (error, trelloCard) {
+            if (error) console.log(error)
+            else console.log('Added card : ' + trelloCard)
+            callback()
+        })
 }
 
-exports.deleteCard = function(cardId, callback){
-    trello.deleteCard(cardId, function(error, result){
+exports.deleteCard = function (cardId, callback) {
+    trello.deleteCard(cardId, function (error, result) {
         if (error) console.log(error)
         else console.log('deleted card: ' + result)
         callback()
     })
 }
 
-exports.updateCardName = function(cardId, name, callback){
-    trello.updateCardName(cardId, name, function(error, result){
+exports.updateCardName = function (cardId, name, callback) {
+    trello.updateCardName(cardId, name, function (error, result) {
         if (error) console.log(error)
         else console.log('Updated card: ' + result)
         callback()
     })
 }
 
-exports.updateListName = function(listId, name, callback){
-    trello.renameList(listId, name, function(error, result){
+exports.updateListName = function (listId, name, callback) {
+    trello.renameList(listId, name, function (error, result) {
         if (error) console.log(error)
         else console.log('Updated list name: ' + result)
         callback()
     })
 }
 
-exports.getBoards = function(callback) {
-
-    trello.getBoards('5dd79b7456259d09ca4fac1a', function(error, result){
+exports.getBoards = function (callback) {
+    trello.getBoards('5dd79b7456259d09ca4fac1a', function (error, result) {
         if (error) console.log(error)
         else console.log(result)
         callback()
     })
 }
 
-exports.addList = function(name, callback){
-    trello.addListToBoard('5df342eb9a2497638c2e7d21', name, function(error, result){
+exports.addList = function (name, callback) {
+    trello.addListToBoard('5df342eb9a2497638c2e7d21', name, function (error, result) {
         if (error) console.log('Error: ' + error)
         else console.log('Added list: ' + result)
         callback()
     })
 }
-exports.getLists = function(callback){
-trello.getListsOnBoard('5df342eb9a2497638c2e7d21', function(error,result){
-    if (error) console.log('error : ' + error)
-    else console.log(result)
-    callback()
-})
+exports.getLists = function (callback) {
+    trello.getListsOnBoard('5df342eb9a2497638c2e7d21', function (error, result) {
+        if (error) console.log('error : ' + error)
+        else console.log(result)
+        callback()
+    })
 }
+
+exports.addTeamMemberToBoard = function(memberId, callback) {
+    trello.addMemberToBoard('5df342eb9a2497638c2e7d21', memberId, function(error, result){
+        if (error) console.log('error : ' + error)
+        else console.log(result)
+        callback(error, result)
+    })
+}
+
+exports.addTeamMemberToCard = function(cardId,memberId,callback) {
+    trello.addMemberToCard(cardId, memberId, function(error, result){
+        if (error) console.log('error : ' + error)
+        else console.log(result)
+        callback(error, result)
+    })
+}
+
+exports.addDueDateToCard = function(cardId, dateValue,callback) {
+    trello.addDueDateToCard(cardId, dateValue, function(error, result){
+        if (error) console.log('error : ' + error)
+        else console.log(result)
+        callback(error, result)
+    })
+
+}
+
+
