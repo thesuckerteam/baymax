0690ae2fd541523503271903778a69398609e050
Oskar Svensson <osksvn@outlook.com>
2019-12-13 16:38:40 +0700

    added trello stuff

---
+++ utility/trello.js
@@ -0,0 +1,40 @@
+var Trello = require('trello');
+
+var trello = new Trello('31d325be1f01a6fc44dc51db6dc63f88', '6d80a3eded49d7a88b16efc5b601745b88a48637159651f6885afc01bf0dde5d');
+
+exports.addCardTODO = function(callback){
+
+    trello.addCard('Clean car', 'Wax on, wax off', '5df342eb9a2497638c2e7d22',
+        function (error, trelloCard) {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log('Added card: ' + trelloCard)
+                callback()
+            }
+        })
+}
+
+exports.getCard = function(callback) {
+
+    trello.getBoards('5dd79b7456259d09ca4fac1a', function(error, result){
+        if (error) console.log(error)
+        else console.log(result)
+        callback()
+    })
+}
+
+exports.addList = function(callback){
+    trello.addListToBoard('5df342eb9a2497638c2e7d21', 'List', function(error, result){
+        if (error) console.log('Error: ' + error)
+        else console.log('Added list: ' + result)
+        callback()
+    })
+}
+exports.getLists = function(callback){
+trello.getListsOnBoard('5df342eb9a2497638c2e7d21', function(error,result){
+    if (error) console.log('error : ' + error)
+    else console.log(result)
+    callback()
+})
+}
