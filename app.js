f8742d121febad97e2d3a99324f181aac8c25744
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2019-12-27 17:08:08 +0700

    add index

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var trelloRouter = require('./routes/trelloRoutes')
+var trelloRouter = require('./routes/trelloRoutes');
+var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/trello', trelloRouter);
 
 // catch 404 and forward to error handler
